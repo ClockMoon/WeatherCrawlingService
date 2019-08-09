d66c97af3cb66d0536e7745c2614d0ff45e5973d
ClockMoon <jiwon3346@naver.com>
2019-08-09 18:56:59 +0900

    백엔드 도메인 주소 수정

--- frontend/util/process.js
+++ frontend/util/process.js
@@ -1,6 +1,6 @@
 const backUrl =
   process.env.NODE_ENV === "production"
-    ? "http://api.pastweathercrawler.tk/"
+    ? "http://api.pastweathercrawler.tk"
     : "http://localhost:8080";
 
 export const backendURL = backUrl;
